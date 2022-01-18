4208156bc703bb6247ffeb5b9c78a9dbab34b703
Alex <realtowz@yahoo.com>
2022-01-18 22:03:08 +0200

    add hostname in model

--- complete/src/main/java/com/example/servingwebcontent/GreetingController.java
+++ complete/src/main/java/com/example/servingwebcontent/GreetingController.java
@@ -1,5 +1,8 @@
 package com.example.servingwebcontent;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,8 +12,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class GreetingController {
 
 	@GetMapping("/greeting")
-	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) throws UnknownHostException {
 		model.addAttribute("name", name);
+		model.addAttribute("host", InetAddress.getLocalHost().getHostName());
 		return "greeting";
 	}
 
